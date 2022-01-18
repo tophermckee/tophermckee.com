de49ccd57d62f391b37a2f8eb40cc58e6d6f34e0
tophermckee <tophermckee@gmail.com>
2022-01-17 19:01:37 -0600

    this is accetable i guess

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+let colors = ['f8f8f2', '8be9fd', '50fa7b', 'ffb86c', 'ff79c6', 'bd93f9', 'ff5555', 'f1fa8c']
 
+let text = 'hello there and welcome to my website. it\'s really nothing fancy, just a place to try out new things like this fun paragraph builder. you should try it out! reload this page and you\'ll see that the colors change every time'
 
-let text = 'this is a line of text that I\'m going to split up'
-
-
-text.split(" ").forEach(word => {
-    let new_div = document.createElement("div")
-    new_div.setAttribute('class', 'item')
-    new_div.innerHTML = word
-
-    document.getElementById("main-container").appendChild(new_div);
-
+document.getElementById('input-field').addEventListener('input', function() {
+    text = document.getElementById('input-field').value
+    
+    document.getElementById("main-container").innerHTML = ''
+    text.split(" ").forEach(word => {
+        let new_div = document.createElement("div")
+        new_div.setAttribute('class', 'item')
+        new_div.setAttribute('style', `color: #${colors[Math.floor(Math.random() * colors.length)]};`)
+        new_div.innerHTML = word
+    
+        document.getElementById("main-container").appendChild(new_div);
+    
+    })
 })
+
